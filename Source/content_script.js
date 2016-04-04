63b606c5cd775e407d5c1296ac5bf957dee674e7
jack-snipe <c77bd85b@opayq.com>
2016-04-04 23:06:39 +0200

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -7,10 +7,10 @@ function walk(node)
 	
 	var child, next;
 	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
+	//if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	//    || node.classList.indexOf('ace_editor') > -1) {
+	//	return;
+	//}
 
 	switch ( node.nodeType )  
 	{
@@ -36,15 +36,26 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bthe Imperium\b/g, "Those Bee Guys (you know, from EVE)");
+	v = v.replace(/\bThe Imperium\b/g, "Those Bee Guys (you know, from EVE)");
+	v = v.replace(/\bImperium\b/g, "Those Bee Guys (you know, from EVE)");
 	v = v.replace(/\bCFC\b/g, "Those Bee Guys (you know, from EVE)");
 	v = v.replace(/\bGoons\b/g, "Those Bee Guys (you know, from EVE)");
+	v = v.replace(/\bgoons\b/g, "Those Bee Guys (you know, from EVE)");
 	v = v.replace(/\bGoonswarm\b/g, "Those Bee Guys (you know, from EVE)");
-	v = v.replace(/\bMittani\b/g, "Queen Bee");
-	v = v.replace(/\bMittens\b/g, "Queen Bee");
-	v = v.replace(/\bThe War of Sovless Aggression\b/g, "World War BEE");
-	v = v.replace(/\bThe Mercenary Wars\b/g, "World War BEE");
+	v = v.replace(/\bgoonswarm\b/g, "Those Bee Guys (you know, from EVE)");
+	
 	v = v.replace(/\bBand of Backstabbers\b/g, "Money Badger Coalition");
+	v = v.replace(/\bband of backstabbers\b/g, "Money Badger Coalition");
+	
+	v = v.replace(/\bMittani\b/g, "Queen Bee");
+	v = v.replace(/\bmittani\b/g, "Queen Bee");
+	v = v.replace(/\bMittens\b/g, "Queen Bee");
+	v = v.replace(/\bmittens\b/g, "Queen Bee");
+	
+	v = v.replace(/\bThe War of Sovless Aggression\b/g, "World War BEE");
+	v = v.replace(/\bthe Mercenary Wars\b/g, "World War BEE");
+	v = v.replace(/\bThe Mercenary Wars\b/g, "World War BEE");
+	v = v.replace(/\bthe mercenary wars\b/g, "World War BEE");
 	
 	textNode.nodeValue = v;
 }
